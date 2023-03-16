a8a0e75b5ff94bf6d91bf366e118bb175288815e
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-16 21:08:42 +0500

    create more test to check it working corrently

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,9 +3,16 @@ require 'rspec'
 
 describe Solver do
     describe "#reserve" do
-        it 'Return the text in reserve order' do
-            solver = Solver.new
-            expect(solver.reserve('Hello')).to eq('olleH')
+          subject { Solver.new }
+        context 'Reverse the text'
+        it 'reserve the Hello to olleh' do
+            expect(subject.reserve('Hello')).to eq('olleH')
+        end
+        it 'reserve the Hello World to dlroW olleH' do
+            expect(subject.reserve('Hello World')).to eq('dlroW olleH')
+        end
+        it 'reserve the word to drow' do
+          expect(subject.reserve('word')).to eq('drow')
         end
     end
 end
