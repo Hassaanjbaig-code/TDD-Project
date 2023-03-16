ac5dce53563ea30400ea9924a4f06f275fdfb26e
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-16 21:43:44 +0500

    use Subject in the code

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,33 +16,27 @@ describe Solver do
         end
     end
     describe "#FizzBuzz" do
+        subject { Solver.new }
           it "Checking the number 3 is fizz" do
-            solver = Solver.new
-            expect(solver.FizzBuzz(3)).to eq("Fizz")
+            expect(subject.FizzBuzz(3)).to eq("Fizz")
           end
           it "Checking the number 9 is fizz" do
-            solver = Solver.new
-            expect(solver.FizzBuzz(9)).to eq("Fizz")
+            expect(subject.FizzBuzz(9)).to eq("Fizz")
           end
           it "Checking the number 5 is buzz" do
-            solver = Solver.new
-            expect(solver.FizzBuzz(5)).to eq("Buzz")
+            expect(subject.FizzBuzz(5)).to eq("Buzz")
           end
           it "Checking the number 10 is buzz" do
-            solver = Solver.new
-            expect(solver.FizzBuzz(10)).to eq("Buzz")
+            expect(subject.FizzBuzz(10)).to eq("Buzz")
           end
           it "Checking the number 15 is fizzbuzz" do
-            solver = Solver.new
-            expect(solver.FizzBuzz(15)).to eq("FizzBuzz")
+            expect(subject.FizzBuzz(15)).to eq("FizzBuzz")
           end
-            it "Checking the number 30 is fizzbuzz" do
-               solver = Solver.new
-               expect(solver.FizzBuzz(30)).to eq("FizzBuzz")
+            it "Checking the number 30 is fizzbuzz" do 
+               expect(subject.FizzBuzz(30)).to eq("FizzBuzz")
             end
             it "Checking the number 2 is 2" do
-                solver = Solver.new
-                expect(solver.FizzBuzz(2)).to eq(2)
-                end
+                expect(subject.FizzBuzz(2)).to eq(2)
+            end
     end
 end
